0fc23d811269eed9a45a124a5ec00838a6f8b22a
rakib <mdrakib5561@gmail.com>
2021-10-29 00:58:24 +0600

    banner added and simple footer added

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -3,8 +3,29 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="footer-wrape">
+            
+            <div className="footer-container">
+            <div>
+                <h3>About</h3>
+            </div>
+
+            <div>
+                <h3>Contact Us</h3>
+            </div>
+            <div>
+                <h3>Support</h3>
+            </div>
+            <div>
+                <h3>We Accepts:</h3>
+            </div>
+            </div>
+
+            <hr />
+            <div className="copy-right">
+                    Copyright 2021 TourX | Design By Md. Rakib
+            </div>
+
         </div>
     );
 };
