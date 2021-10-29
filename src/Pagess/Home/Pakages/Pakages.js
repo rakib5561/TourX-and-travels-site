86aad5eb50f8ca98dff6743f46a733145777d278
rakib <mdrakib5561@gmail.com>
2021-10-29 20:04:52 +0600

    servicess added

--- src/Pagess/Home/Pakages/Pakages.js
+++ src/Pagess/Home/Pakages/Pakages.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Pakages.css';
 
 const Pakages = () => {
+
+
+    const [pakages, setPakages] = useState([]);
+
+    useEffect( ()=>{
+
+        fetch('pakages.JSON')
+        .then(res => res.json())
+        .then(data => setPakages(data))
+
+    },[]);
+
+
     return (
         <div>
             <div className="Tour-pakages-wrap">
@@ -9,6 +23,25 @@ const Pakages = () => {
             <div className="pakages-content">
                 <h5>Choose Your Package</h5>
                 <h1>Select Your Best Package <br /> For Your Travel</h1>
+            </div>
+
+            <div className="card-container">
+                {
+                    pakages.map(pakage => (
+                        <div className="card-style">
+                            <img src={pakage.image} alt="" />
+
+                            <div className="package-price-duration">
+                                <div>{pakage.price}</div>
+                                <div>{pakage.duration}</div>
+                            </div>
+                            <h5 className="package-title">{pakage.title}</h5>
+
+                            <Link to=''><button className="btn-style">Booking Now</button></Link>
+
+                        </div>
+                    ))
+                }
             </div>
 
             </div>
