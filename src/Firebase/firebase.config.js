0dcf6eed89850f588cde0132631ff0820e04c132
rakib <mdrakib5561@gmail.com>
2021-11-01 12:23:08 +0600

    readmi file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const firebaseConfig = {
-    apiKey:`${process.env.procREACT_API_KEY}`,
-    authDomain: "tourx-6a075.firebaseapp.com",
-    projectId: "tourx-6a075",
-    storageBucket: "tourx-6a075.appspot.com",
-    messagingSenderId: "214392073438",
-    appId: "1:214392073438:web:738695049bef6d358ca64f"
+  apiKey: "AIzaSyAelQonqd4E20vTU9o8B1gzKfWrb_Ia4cs",
+  authDomain: "tourx-6a075.firebaseapp.com",
+  projectId: "tourx-6a075",
+  storageBucket: "tourx-6a075.appspot.com",
+  messagingSenderId: "214392073438",
+  appId: "1:214392073438:web:738695049bef6d358ca64f"
   };
 
 export default firebaseConfig;
